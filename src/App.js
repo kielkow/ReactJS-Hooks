9330a046922e8494128c12401d522a3423bdfafd
kielkow <matheuskiel@fiorifer.com.br>
2019-12-10 20:36:30 -0300

    Hook useState and Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [tech, setTech] = useState([]);
+
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }
+
+  // Like componentDidMount
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (tech)
+      setTech(JSON.parse(storageTech))
+  }, [])
+
+  // Like componentDidUpdate
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <ul>
+      { tech.map(t => (<li key={t}>{t}</li>))}
+    </ul>
+    <input type="text" value={newTech} onChange={e => setNewTech(e.target.value)}/>
+    <button type="button" onClick={handleAdd}>Add</button>
+    </>
   );
 }
 
